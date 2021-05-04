aa8013f8c03936fc5b6b46a7770151e6c053642b
AzaharUddinEmon <azaharuddinemon@gmail.com>
2021-05-04 23:30:15 +0600

    uploading AWS Storages

--- album/models.py
+++ album/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Album(models.Model):
 
-    thumbnail = models.ImageField(upload_to='album/photo/')
+    thumbnail = models.ImageField(upload_to='album/photo/azaharudddin/')
     description = models.TextField()
     creation = models.DateTimeField(auto_now_add=True)
 
--- testproject/settings.py
+++ testproject/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
 
     #my_apps
     'myapp',
-    'album'
+    'album',
+    'storages'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'testproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [ BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,14 +126,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
+STATICFILES_DIR = [ BASE_DIR / 'static']
+STATIC_ROOT =  BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+MEDIA_ROOT = BASE_DIR / 'media/'
 
-STATIC_ROOT = [BASE_DIR / 'staticfiles']
-STATICFILES_DIRS = [BASE_DIR / 'static']
+
+AWS_ACCESS_KEY_ID = 'AKIARVGPJVYVGB6OFLTK'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'nasim.secpstorage'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
